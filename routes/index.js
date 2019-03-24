35a0eaf56bf06d8b1671a2b353c1a559e7c29df5
ZakariaAourir <zakaria1997aourir@gmail.com>
2019-03-24 18:23:09 +0100

    updates

--- routes/index.js
+++ routes/index.js
@@ -128,26 +128,26 @@ function isLoggedIn(req, res, next) {
 // nodemailer - post -
 router.post('/send',function(req,res,next){
   var transporter  = nodemailer.createTransport({
-    service:"Gmail",
-    auth:{
-      user:'zakaria1997aourir@gmail.com',
-      pass:'zakaria1234567'
+    service: "Gmail",
+    auth: {
+      user: "zakaria1997aourir@gmail.com",
+      pass: "zakaria1234567"
     }
   });
 
   var mailOptions = {
-    from:'"Zakaria Aourir" <zakaria1997aourir@gmail.com>',
-    to:'zakaria1997aourir@hotmail.com',
-    subject:"Intership Online",
-    text:'you have a submission from... Name: '+req.body.name+' Email: '+req.body.email+'Message: '+req.body.message+'',
-    html:'<p>you have a submission from...</p> <ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
+    from: '"Zakaria Aourir ?" <zakaria1997aourir@gmail.com>',
+    to: 'zaourir97@gmail.com',
+    subject: "Intership Online",
+    text: 'you have a submission from... Name: '+req.body.name+' Email: '+req.body.email+'Message: '+req.body.message+'',
+    html: '<p>you have a submission from...</p> <ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
   }
   transporter.sendMail(mailOptions,function(error,info){
     if(error){
       return console.log(error);
     }
     console.log('message sent'+info.response);
-    res.redirect('/');
+    res.redirect('/homepage');
   });
 
 });
